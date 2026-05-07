d9146cf23c79df3a09cf0bd7d5ed441f24829b63
Vansh Thakur <vthakur.290905@gmail.com>
2026-05-07 11:50:08 +0530

    removed flutter files and folder as moving to React Native
    removed flutter files and folder as moving to React Native

--- lib/core/config/app_config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  AppConfig._();
-
-  // Replace with your own NewsAPI key.
-  static const String newsApiKey = '5224366499d848469dde94dbc983158a';
-}
--- lib/core/constants/api_endpoints.dart
+++
@@ -1,6 +0,0 @@
-class ApiEndpoints {
-  ApiEndpoints._();
-
-  static const String baseUrl = 'https://newsapi.org/v2';
-  static const String topHeadlines = '/top-headlines';
-}
