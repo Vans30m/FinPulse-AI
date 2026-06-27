db0271a3e344c3145bc85e6aad873be3dab3df3b
Vansh Thakur <vthakur.290905@gmail.com>
2026-06-27 19:36:52 +0530

    removed some files as they were of no use now
    removed some files as they were of no use now

--- finpulse-web/backend/src/routes/newsRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchNewsSentiment } from "../controllers/newsController.js";
-
-const router = express.Router();
-
-router.get("/sentiment", fetchNewsSentiment);
-
-export default router;
