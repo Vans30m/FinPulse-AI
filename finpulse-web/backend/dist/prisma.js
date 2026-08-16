025cd32e5dd3c251ac18275daf78542e792ce08d
Vansh Thakur <vthakur.290905@gmail.com>
2026-08-16 20:25:58 +0530

    Ignore generated backend build files

--- finpulse-web/backend/dist/prisma.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import pg from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import 'dotenv/config';
-const connectionString = process.env.DATABASE_URL;
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-export const prisma = new PrismaClient({ adapter });
